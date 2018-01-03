74b8e196465535ec2a403b6d16c46a64527d9ffe
Jacob Montpetit <vikingfacer@users.noreply.github.com>
2018-01-02 20:46:21 -0500

    basic set up

---
+++ raspPins/target/armv7-unknown-linux-gnueabihf/debug/deps/raspPins-e5e983b776ac3d8b.d
@@ -0,0 +1,5 @@
+/Users/jacobmontpetit/RustStuff/rustPi/rusty_bot/rusty_bot/raspPins/target/armv7-unknown-linux-gnueabihf/debug/deps/raspPins-e5e983b776ac3d8b: src/main.rs
+
+/Users/jacobmontpetit/RustStuff/rustPi/rusty_bot/rusty_bot/raspPins/target/armv7-unknown-linux-gnueabihf/debug/deps/raspPins-e5e983b776ac3d8b.d: src/main.rs
+
+src/main.rs:
---
+++ raspPins/target/debug/raspPins.d
@@ -0,0 +1,1 @@
+/Users/jacobmontpetit/RustStuff/rustPi/rusty_bot/rusty_bot/raspPins/target/debug/raspPins.dSYM: /Users/jacobmontpetit/RustStuff/rustPi/rusty_bot/rusty_bot/raspPins/src/main.rs
