ac5902f5711c1658f4af9325de434c6b578434d4
Jacob Montpetit <vikingfacer@users.noreply.github.com>
2018-01-02 21:10:51 -0500

    blink

--- raspPins/src/main.rs
+++ raspPins/src/main.rs
@@ -1,3 +1,21 @@
+extern crate cupi;
+
+use cupi::{CuPi, delay_ms, DigitalWrite};
+
 fn main() {
-    println!("Hello, world!");
+    let cupi = CuPi::new().unwrap();
+    let mut pinout = cupi.pin(0).unwrap().high().output();
+    //let mut pin = cupi.pin_sys(0).unwrap();
+    //pin.export().unwrap();
+    //let mut pinout = pin.output().unwrap();
+
+    for _ in 0..20 {
+        pinout.high().unwrap();
+        delay_ms(600);
+        pinout.low().unwrap();
+        delay_ms(600);
+    }
 }
+
+
+
