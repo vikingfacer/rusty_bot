a5ef155e919f3f6e347f3ce47da2450b9d2d3032
Jacob Montpetit <vikingfacer@users.noreply.github.com>
2018-01-17 20:29:58 -0500

    added spi messager

--- raspPins/src/main.rs
+++ raspPins/src/main.rs
@@ -1,24 +1,90 @@
-extern crate rust_pigpio;
-extern crate libc;
+// extern crate i2cdev;
 
-use rust_pigpio::*;
+// use std::thread;
+// use std::time::Duration;
 
-use std::fs::File;
-use std::io::prelude::*;
+// use i2cdev::core::*;
+// use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
 
+// const SLAVE_ADDR: u16 = 0x04;
 
+// // real code should probably not use unwrap()
+// fn main() {
+//     let mut dev = LinuxI2CDevice::new("/dev/i2c-1", SLAVE_ADDR).unwrap();
+//     let mut r_buf :[u8; 6] = [0; 6];
+//     // init sequence
+//     dev.smbus_write_byte_data(0x04, 1).unwrap();
+//     dev.smbus_write_byte_data(0x04, 2).unwrap();
+//     thread::sleep(Duration::from_millis(10));
 
-const ADDRESS : u32 = 0x04;
-const I2_C_DEVICE : & str = "dev/i2c-0"; 
+//     dev.read(&mut r_buf).unwrap();
+//     println!("{:?}",r_buf );
+// }
 
-//Turns light on and off
-fn main() {
+extern crate spidev;
+use spidev::{Spidev, SpidevOptions, SPI_MODE_0};
+use spidev::spidevioctl::SpidevTransfer;
+use std::io;
+use std::mem;
 
-  let f = File::open(I2_C_DEVICE);
+fn parse_message(raw_mess : String) -> Vec<u8>{
+	let mut message : Vec<u8>= Vec::new();
 
-  println!("Initialized pigpio. Version: {}", initialize().unwrap());
+	for word in raw_mess.split_whitespace(){
 
-
-
-  terminate();
+		match word.trim().parse(){
+			Ok(my_num) => message.push(my_num),
+			Err(_) => continue,
+			};
+	}
+	message.push(0xA);
+	message
 }
+
+fn main() {
+    let mut spidev = Spidev::open("/dev/spidev0.0").unwrap();
+    let options = SpidevOptions::new()
+                      .bits_per_word(8)
+                      .max_speed_hz(5000)
+                      .lsb_first(false)
+                      .mode(SPI_MODE_0)
+                      .build();
+    spidev.configure(&options).unwrap();
+    loop {
+        
+	    println!("===== Single transfer =========");
+
+		let mut input = String::new();
+		match io::stdin().read_line(&mut input) {
+		    Ok(n) => {
+		        println!("{} bytes read", n);
+		        println!("{}", input);
+		    }
+		    Err(error) => println!("error: {}", error),
+		}
+		// for word in input.split(' '){
+		// 	println!("split: {:?}", word);
+		// }
+		// let my_num : u8 = match input.trim().parse(){
+		// 	Ok(my_num) => my_num,
+		// 	Err(_) => continue,
+		// };
+
+		let mut dev_buf : Vec<u8>= parse_message(input);
+		println!("{:?}",dev_buf );
+		// dev_buf.push(my_num);
+		// dev_buf.push(0xA);
+
+		// let mut read_buf= vec!(&dev_buf[..]);
+
+	    let mut trans = SpidevTransfer::write(&dev_buf[..]);
+
+
+	    println!("{:?}", spidev.transfer(&mut trans));
+	}
+}
+
+
+
+
+
---
+++ raspPins/target/debug/build/i2cdev-685c712fa54bdb0f/out/skeptic-tests.rs
@@ -0,0 +1,35 @@
+extern crate skeptic;
+
+#[allow(dead_code)] fn readme_0() {
+    let ref s = format!("{}", r####"extern crate i2cdev;
+
+use std::thread;
+use std::time::Duration;
+
+use i2cdev::core::*;
+use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
+
+const NUNCHUCK_SLAVE_ADDR: u16 = 0x52;
+
+// real code should probably not use unwrap()
+fn i2cfun() -> Result<(), LinuxI2CError> {
+    let mut dev = try!(LinuxI2CDevice::new("/dev/i2c-1", NUNCHUCK_SLAVE_ADDR));
+
+    // init sequence
+    try!(dev.smbus_write_byte_data(0xF0, 0x55));
+    try!(dev.smbus_write_byte_data(0xFB, 0x00));
+    thread::sleep(Duration::from_millis(100));
+
+    loop {
+        let mut buf: [u8; 6] = [0; 6];
+        dev.smbus_write_byte(0x00).unwrap();
+        thread::sleep(Duration::from_millis(10));
+        dev.read(&mut buf).unwrap();
+        println!("Reading: {:?}", buf);
+    }
+}
+"####);
+    skeptic::rt::run_test(r#"/Users/jacobmontpetit/RustStuff/rustPi/rusty_bot/rusty_bot/raspPins/target/debug/build/i2cdev-685c712fa54bdb0f/out"#, s);
+}
+
+
